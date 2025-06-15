5d40e5b99b498b93c2a982333c7e07dd3e850a57
Neko Ayaka <neko@ayaka.moe>
2025-06-16 03:03:15 +0800

    chore(deps): bump dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,24 @@
 import antfu from '@antfu/eslint-config'
+import { importX} from 'eslint-plugin-import-x'
 
 export default antfu({
-  formatters: true,
+  rules: {
+    'ts/ban-ts-comment': 'off',
+    'antfu/import-dedupe': 'error',
+    'style/padding-line-between-statements': 'error',
+    'import-x/order': [
+      'error',
+      {
+        'groups': [
+          ['type'],
+          ['builtin', 'external'],
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  plugins: {
+    'import-x': importX,
+  },
 })
